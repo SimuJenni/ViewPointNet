d770e85f1bbc7f59167d2310cfbd348360ed76a9
SimuJenni <simujenni@gmail.com>
2017-04-26 11:26:12 +0200

    try another setup

--- test_samples.py
+++ test_samples.py
@@ -21,6 +21,7 @@ with tf.Session() as sess:
 
     # Preprocess data
     img_processed = preprocessor.process_train(img, box, im_size)
+    img = tf.image.resize_bilinear(img, [96, 96], align_corners=False)
     imgs, imgs_processed = tf.train.batch([img, img_processed], batch_size=bs, num_threads=1)
 
     names_to_values, names_to_updates = slim.metrics.aggregate_metric_map({
